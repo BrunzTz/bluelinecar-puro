c7b176536d28f2ebf78fd4a93d0e927067470921
Luis-Macedo <luismacedo2001@outlook.com>
2021-04-15 10:41:56 -0300

    Interface de Login

--- src/Pages/login/loginSeller.php
+++ src/Pages/login/loginSeller.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
-
 <html lang="pt-br">
-
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,13 +9,13 @@
         <link rel="stylesheet" href="../../style/navbar/navbar.css">
         <link rel="stylesheet" href="../../style/modelos/modelos.css">
         <link rel="stylesheet" href="../../style/footer/footer.css">
+        <link rel="stylesheet" href="../../style/signup/userSignup.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <title>Bluelinecar</title>
+        <title>Login</title>
     </head>
-
     <body>
 
-        <!-- Navbar -->
+        <!--Navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient">
             <img class="navbar-brand logo-size" src="../../assets/images/logo.png" alt="">
 
@@ -48,11 +46,11 @@
                         <div class="btn-group mr-3" dropdown>
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item dropdown">
-                                    <buton class="btn btn-primary dropdown-toggle size-button" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a class="btn btn-primary dropdown-toggle size-button" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         Ações
-                                    </button>
+                                    </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="list.php">Vendedores</a>
+                                        <a class="dropdown-item" href="../vendedores/list.php">Vendedores</a>
                                         <a class="dropdown-item" href="#">Clientes</a>
                                         <a class="dropdown-item" href="#">Veículos</a>
                                     <div class="dropdown-divider"></div>
@@ -69,9 +67,9 @@
                                         Login
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="loginDropdown">
-                                        <a class="dropdown-item" href="../login/loginSeller.php">Vendedores</a>
+                                        <a class="dropdown-item" href="./loginSeller.php">Vendedores</a>
                                     <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="../login/loginUser.php">Clientes</a>
+                                        <a class="dropdown-item" href="./loginUser.php">Clientes</a>
                                     </div>
                                 </li>
                             </ul>
@@ -83,21 +81,42 @@
             
         </nav>
 
-        <!-- Content -->
+        <!--Formulário-->
         <div class="container">
-            
-            <!-- Listagem de Vendedores -->
-            <h1 style="color: black">Vendedores</h1>
+            <div class="row justify-content-md-center">
+                <form class="col-10 formulario">
+                
+                    <!-- Email input -->
+                    <div class="form-outline m-4">
+                        <label class="form-label" for="email">Código</label>
+                        <input type="email" id="email" class="form-control" required/>
+                    </div>
 
+                    <!-- Password input -->
+                    <div class="form-outline m-4">
+                        <label class="form-label" for="senha">Senha</label>
+                        <input type="password" id="senha" class="form-control" required/>
+                    </div>
+
+                    <div class="custom-control custom-checkbox m-4">
+                        <input type="checkbox" class="custom-control-input" id="defaultUnchecked">
+                        <label class="custom-control-label" for="defaultUnchecked">Lembrar usuário e senha</label>
+                    </div>
+
+                    <!-- Submit button -->
+                    <button type="submit" class="btn btn-primary btn-lg mb-4 mr-4 ml-4">Login</button>
+                </form>
+            
+            </div>
         </div>
-        
-        <!-- Footer -->
-        <?php require '../../shared/footer/footer.php'; ?>
+
+        <!--footer-->
+        <?php
+            require '../../shared/footer/footer.php';
+        ?>
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    
     </body>
-
 </html>
