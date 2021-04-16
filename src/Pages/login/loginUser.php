a2007c7bdfec0df509585ea09e60f98379cc9ec4
BrunzTz <55399785+BrunzTz@users.noreply.github.com>
2021-04-15 21:12:29 -0300

    Melhoria visual do login

--- src/Pages/login/loginUser.php
+++ src/Pages/login/loginUser.php
@@ -15,75 +15,10 @@
     </head>
     <body>
 
-        <!--Navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient">
-            <img class="navbar-brand logo-size" src="../../assets/images/logo.png" alt="">
-
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse navbar-container" id="navbarSupportedContent">
-                    
-                    <div>
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item mr-3">
-                                <a class="nav-link" href="../../index.php">Home<span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item mr-3">
-                                <a class="nav-link" href="#">Modelos</a>
-                            </li>
-                            <li class="nav-item mr-3">
-                                <a class="nav-link" href="#">Avaliações</a>
-                            </li>
-                            <li class="nav-item mr-3">
-                                <a class="nav-link" href="#">Sobre</a>
-                            </li>
-                        </ul>
-                    </div>
-                    
-                    <div>
-                        <div class="btn-group mr-3" dropdown>
-                            <ul class="navbar-nav mr-auto">
-                                <li class="nav-item dropdown">
-                                    <a class="btn btn-primary dropdown-toggle size-button" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Ações
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="../vendedores/list.php">Vendedores</a>
-                                        <a class="dropdown-item" href="#">Clientes</a>
-                                        <a class="dropdown-item" href="#">Veículos</a>
-                                    <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Vendas</a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="btn-group mr-3" dropdown>
-                            <ul class="navbar-nav mr-auto">
-                                <li class="nav-item dropdown">
-                                    <a class="btn btn-success dropdown-toggle size-button" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Login
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="loginDropdown">
-                                        <a class="dropdown-item" href="./loginSeller.php">Vendedores</a>
-                                    <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="./loginUser.php">Clientes</a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
-            
-        </nav>
-
         <!--Formulário-->
-        <div class="container">
-            <div class="row justify-content-md-center">
+        <div class="container-login">
+            <div class="content-login">
+                <img  class="logo-login" src="../../assets/images/logo.png" alt="">
                 <form class="col-10 formulario">
                 
                     <!-- Email input -->
@@ -106,14 +41,8 @@
                     <!-- Submit button -->
                     <button type="submit" class="btn btn-primary btn-lg mb-4 mr-4 ml-4">Login</button>
                 </form>
-            
             </div>
         </div>
-
-        <!--footer-->
-        <?php
-            require '../../shared/footer/footer.php';
-        ?>
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
